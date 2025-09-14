f98ad0ebd3d726ac01cc6ab02c32f883b04f035c
Festus Nzuma <mutindafestus27@gmail.com>
2025-09-14 12:07:11 +0300

    fixed finman name error

--- api/requirements.R
+++ api/requirements.R
@@ -3,6 +3,6 @@
 install.packages(c("plumber","mongolite","httr","jsonlite","jose","sodium","tidyverse","future","promises","rlang","remotes"))
 
 # install finman
-remotes::install_github("statisticsguru1/personal-finance-manager@v1.2.0", 
+remotes::install_github("statisticsguru1/personal-finance-manager", 
                         subdir = "finman", 
                         upgrade = "never")
